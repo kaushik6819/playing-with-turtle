1c745cde1d0e5bfe67f91a0d997c0117a0537e7f
kaushik6819 <kaushikkumar200410@gmail.com>
2025-12-10 23:01:55 +0530

    i make a linedash by using turtle

---
+++ dash.py
@@ -0,0 +1,18 @@
+from turtle import Turtle,Screen
+
+
+timmy = Turtle()
+
+
+for _ in range(15):
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+    timmy.forward(10)
+
+
+
+
+my_screen = Screen()
+my_screen.canvheight
+my_screen.exitonclick()
