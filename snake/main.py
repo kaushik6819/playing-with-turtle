c4093fd661281b42afa8c1ae810e8c0ec8a30cdb
kaushik6819 <kaushikkumar200410@gmail.com>
2025-12-20 00:14:05 +0530

    new file created for snake food and scoreboard

--- snake/main.py
+++ snake/main.py
@@ -1,6 +1,8 @@
 import turtle
 import time
 from snake import Snake
+from food import Food
+from score_board import Score
 
 screen = turtle.Screen()
 turtle.setup(height=600, width=600)
@@ -9,6 +11,9 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 anish = Snake()
+poison = Food()
+score = Score()
+
 screen.listen()
 screen.onkey(anish.Up,"Up")
 screen.onkey(anish.Down,"Down")
@@ -21,6 +26,16 @@ while flag:
     screen.update()
     time.sleep(0.2)
     anish.move()
+
+    #collision detection 
+    if anish.head.distance(poison) < 15:
+
+        poison.refresh()
+        score.count_score()
+    if (anish.head.xcor() > 300 or anish.head.xcor() < -300 or anish.head.ycor() > 300 or anish.head.ycor()< -300):
+        score.game_over()
+        flag = False
+    
 
 
     
---
+++ snake/score_board.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+FONT = ("Arial",13,"normal")
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.count = 0
+        self.color("white")
+        
+        self.hideturtle()
+        self.penup()
+        self.goto(0,280)
+        self.update()
+    
+    def update(self):
+        self.write(f"SCORE :{self.count}",True,font=FONT, align="center")
+    
+    def game_over(self):
+        self.home()
+        self.write(f"GAME OVER",True,font=FONT, align="center")
+    
+
+    def count_score(self):
+        self.clear()
+        self.count+=1
+        self.goto(0,280)
+        self.update()
+        
+        
+
+
+    
